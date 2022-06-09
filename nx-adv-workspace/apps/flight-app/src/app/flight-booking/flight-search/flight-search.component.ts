6277e09c2f366212f0ce10d32e7d63e5c14aeccb
mikezks <23002661+mikezks@users.noreply.github.com>
2022-06-09 15:26:54 +0200

    feat: expression changed after it has been checked

--- nx-adv-workspace/apps/flight-app/src/app/flight-booking/flight-search/flight-search.component.ts
+++ nx-adv-workspace/apps/flight-app/src/app/flight-booking/flight-search/flight-search.component.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable @angular-eslint/no-empty-lifecycle-method */
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Flight } from '@flight-workspace/flight-lib';
 import { Store } from '@ngrx/store';
 import { delay, of } from 'rxjs';
@@ -17,13 +17,14 @@ import { FlightStoreService } from './flight-store.service';
     FlightStoreService
   ]
 })
-export class FlightSearchComponent implements OnInit {
+export class FlightSearchComponent implements OnInit, AfterViewInit {
 
   from = 'Hamburg'; // in Germany
   to = 'Graz'; // in Austria
   urgent = false;
   flights$ = this.globalStore.select(fromFlightBooking.selectActiveUserFlights);
   vm$ = this.localStore.selectViewModel$;
+  evilProperty = 0;
 
   // "shopping basket" with selected flights
   basket: { [id: number]: boolean } = {
@@ -35,6 +36,21 @@ export class FlightSearchComponent implements OnInit {
     private globalStore: Store<fromFlightBooking.FlightBookingRootState>,
     private localStore: FlightStoreService) {
 
+    this.evilProperty = 1;
+  }
+
+  ngAfterViewInit(): void {
+    /**
+     * Abarbeitung in aynchrone Event-Verarbeitung verschieben
+     * -> neuer ApplicationRef.tick() ChangeDetection Run
+     *
+     *  -- oder --
+     *
+     * Vor dem Binding der Daten abarbeiten lassen
+     */
+    setTimeout(
+      () => this.evilProperty = 2
+    );
   }
 
   ngOnInit() {
