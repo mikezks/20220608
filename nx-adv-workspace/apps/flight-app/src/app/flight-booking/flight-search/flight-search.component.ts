3c7279bedfaa95110e58f6f129f61cbde5bf59c8
mikezks <23002661+mikezks@users.noreply.github.com>
2022-06-09 11:16:06 +0200

    feat: local state management

--- nx-adv-workspace/apps/flight-app/src/app/flight-booking/flight-search/flight-search.component.ts
+++ nx-adv-workspace/apps/flight-app/src/app/flight-booking/flight-search/flight-search.component.ts
@@ -1,22 +1,45 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable @angular-eslint/no-empty-lifecycle-method */
 import { Component, OnInit } from '@angular/core';
-import { Flight } from '@flight-workspace/flight-lib';
+import { Flight, FlightService } from '@flight-workspace/flight-lib';
+import { ComponentStore } from '@ngrx/component-store';
 import { Store } from '@ngrx/store';
+import { delay, map, Observable, of, switchMap, tap, withLatestFrom } from 'rxjs';
 import * as fromFlightBooking from '../+state';
+
+
+interface Filter {
+  from: string;
+  to: string;
+  urgent: boolean;
+}
+
+interface LocalState {
+  filters: Filter[];
+  flights: Flight[];
+}
+
+const initialLocalState: LocalState = {
+  filters: [],
+  flights: []
+}
+
 
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'flight-search',
   templateUrl: './flight-search.component.html',
-  styleUrls: ['./flight-search.component.css']
+  styleUrls: ['./flight-search.component.css'],
+  providers: [
+    ComponentStore
+  ]
 })
 export class FlightSearchComponent implements OnInit {
 
   from = 'Hamburg'; // in Germany
   to = 'Graz'; // in Austria
   urgent = false;
-  flights$ = this.store.select(fromFlightBooking.selectActiveUserFlights);
+  flights$ = this.globalStore.select(fromFlightBooking.selectActiveUserFlights);
 
   // "shopping basket" with selected flights
   basket: { [id: number]: boolean } = {
@@ -24,16 +47,98 @@ export class FlightSearchComponent implements OnInit {
     5: true
   };
 
-  constructor(private store: Store<fromFlightBooking.FlightBookingRootState>) {
+  /**
+   * Updaters
+   */
+
+  addFilter = this.localStore.updater(
+    (state, filter: Filter) => ({
+      ...state,
+      filters: [
+        ...state.filters,
+        filter
+      ]
+    })
+  );
+
+  setFlights = this.localStore.updater(
+    (state, flights: Flight[]) => ({
+      ...state,
+      flights
+    })
+  );
+
+  /**
+   * Selectors
+   */
+
+  selectFilters$ = this.localStore.select(
+    state => state.filters
+  );
+
+  selectFlights$ = this.localStore.select(
+    state => state.flights
+  );
+
+  selectViewModel$ = this.localStore.select(
+    // Selectors
+    this.selectFilters$,
+    this.selectFlights$,
+    (filters, flights) => ({
+      flights,
+      filter: filters[filters.length-1]
+    })
+  );
+
+  /**
+   * Effects
+   */
+
+  searchFlights = this.localStore.effect(
+    (trigger$: Observable<void>) =>
+      trigger$.pipe(
+        withLatestFrom(this.selectFilters$),
+        map(([, filters]) => filters[filters.length-1]),
+        switchMap(filter => this.flightService.find(
+          filter.from,
+          filter.to,
+          filter.urgent
+        )),
+        tap(flights => this.setFlights(flights))
+      )
+  );
+
+  constructor(
+    private globalStore: Store<fromFlightBooking.FlightBookingRootState>,
+    private localStore: ComponentStore<LocalState>,
+    private flightService: FlightService) {
+
+    this.localStore.setState(initialLocalState);
   }
 
   ngOnInit() {
+
+    this.addFilter(of({
+      from: 'London',
+      to: 'New York',
+      urgent: this.urgent
+    }).pipe(
+      delay(3_000)
+    ));
   }
 
   search(): void {
     if (!this.from || !this.to) return;
 
-    this.store.dispatch(
+    this.addFilter({
+      from: this.from,
+      to: this.to,
+      urgent: this.urgent
+    });
+
+    this.searchFlights();
+
+    this.globalStore.dispatch(
       fromFlightBooking.flightsLoad({
         from: this.from,
         to: this.to,
@@ -43,7 +148,7 @@ export class FlightSearchComponent implements OnInit {
   }
 
   delay(flight: Flight): void {
-    this.store.dispatch(
+    this.globalStore.dispatch(
       fromFlightBooking.flightUpdate({
         flight: {
           ...flight,
